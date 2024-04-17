70d5949d2de022af52fc30c28fb1689ee5ada697
VanUffelenAntoine <antoine@mansio.be>
2024-04-17 15:43:18 +0200

    Added default-theme Added sending messages w:o response

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
+import React, { useCallback, useState } from "react";
 import { UserPath } from "./components/UserPath";
 import "./index.css";
 
 function App() {
+  const [inputVal, setInputVal] = useState("");
+  const [history, setHistory] = useState<string[]>([]);
+  console.log(inputVal);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputVal(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setHistory([...history, inputVal]);
+    setInputVal("");
+  };
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello wkekwsld!</h1>
-      <UserPath />
-    </>
+    <div className="font-mono bg-background text-foreground h-screen">
+      <div id="history">
+        {history.map((item) => {
+          return (
+            <div className="flex flex-row">
+              <UserPath />
+              <div>{item}</div>
+            </div>
+          );
+        })}
+      </div>
+      <form className="flex flex-row" onSubmit={handleSubmit}>
+        <UserPath />
+        <input
+          className=" px-2 bg-transparent outline-none bg-background text-foreground"
+          title="terminal-input"
+          type="text"
+          id="terminal-input"
+          autoComplete="off"
+          spellCheck="false"
+          autoFocus
+          autoCapitalize="off"
+          value={inputVal}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
   );
 }
 
--- src/components/UserPath.tsx
+++ src/components/UserPath.tsx
@@ -5,12 +5,14 @@ export const UserPath = () => {
 
   return (
     <div className="font-bold flex">
-      <span>{user}</span>
-      <span>@</span>
-      <span>{domain}</span>
-      <span>: </span>
-      <span>{path}</span>
-      <span>$ </span>
+      <span className="text-yellow">{user}</span>
+      <span className="text-white">@</span>
+      <span className="text-green">{domain}</span>
+      <span className="text-white">
+        <span>: </span>
+        <span>{path}</span>
+        <span>$ </span>
+      </span>
     </div>
   );
 };
